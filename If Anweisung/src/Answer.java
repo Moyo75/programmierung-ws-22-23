c18a590f24fadd1ff31c8580e7315b3eaf3bf6e3
Moyo75 <moyojohn188@gmail.com>
2022-10-24 21:42:16 +0200

    "Antwort auf Alles" solved

--- If Anweisung/src/Answer.java
+++ If Anweisung/src/Answer.java
@@ -4,18 +4,20 @@ public class Answer {
 	 * Die main Methode verschiedene Aufrufe testen
 	 */
 	public static void main(String[] args) {
-		System.out.println(Answer.theAnswer(42));//sollte true zurueckgeben
-		System.out.println(Answer.theAnswer(27));//sollte false zurueckgeben
+		System.out.println(Answer.theAnswer(42));// sollte true zurueckgeben
+		System.out.println(Answer.theAnswer(27));// sollte false zurueckgeben
 	}
-	
+
 	/*
-	 * Diese Methode soll zurueckgeben, ob der übergeben Parameter 42 ist.
-	 * Wenn dies der Fall ist, dann soll true zurückgegeben werden, ansonsten false.
+	 * Diese Methode soll zurueckgeben, ob der übergeben Parameter 42 ist. Wenn dies
+	 * der Fall ist, dann soll true zurückgegeben werden, ansonsten false.
 	 */
-	public static boolean theAnswer(int zahl)
-	{
-		//Hier beginnt der zu bearbeitende Bereich
+	public static boolean theAnswer(int zahl) {
+		// Hier beginnt der zu bearbeitende Bereich
+		if (zahl == 42) {
+			return true;
+		}
 		return false;
-		//Hier endet der zu bearbeitende Bereich
+		// Hier endet der zu bearbeitende Bereich
 	}
 }
